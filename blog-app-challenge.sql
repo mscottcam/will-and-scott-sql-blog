4ef8074ab4a5bdbde13a3b58aeccfe7134eeb878
52lions06 <mckelvey.william@yahoo.com>
2017-07-05 16:28:34 -0500

    updated

--- blog-app-challenge.sql
+++ blog-app-challenge.sql
@@ -1,19 +1,43 @@
 CREATE TABLE users (
-  id serial PRIMARY KEY
-  first_name text
-  last_name text
-  email text NOT NULL
+  id serial PRIMARY KEY,
+  first_name text,
+  last_name text,
+  email text NOT NULL,
   screen_name text NOT NULL
 );
 
 CREATE TABLE posts (
-  id serial PRIMARY KEY
-  author_id int REFERENCES users
-  title text
-  content text
-  published timestamp DEFAULT now()
+  id serial PRIMARY KEY,
+  author_id int REFERENCES users,
+  title text,
+  content text,
+  published timestamp DEFAULT now(),
+  comments, 
+  tags
+);
+
+CREATE TABLE comments (
+ id serial PRIMARY KEY,
+ author_id int REFERENCES users,
+ post_id int REFERENCES posts,
+ comment text NOT NULL
+);
+
+CREATE TABLE tags (
+  current events text 
 )
 
 -- make table for tags
 -- table for tags that belong to post
 -- table for comments
+
+INSERT INTO users (first_name,last_name, email, screen_name);
+VALUES (Allen, Poe, theRaven@chamberdoor.com, heartbreaker001), (William, Shakespere, RomeoGoingHAM@aol.com, theGOAT123)
+
+INSERT INTO posts (author_id int REFERENCES users,
+  title text,
+  content text,
+  published timestamp DEFAULT now(),
+  comments, 
+  tags
+);
